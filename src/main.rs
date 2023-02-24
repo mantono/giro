fd1a862945d0da6d717f3fed7ed7b41ea325ee6d
Anton Österberg <anton.osterberg@protonmail.com>
2023-02-24 01:35:18 +0100

    Provide better and clear output to user for binary

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use is_git::git_root;
 
 fn main() {
     let mut args = std::env::args();
-    let path = args.nth(1).unwrap_or(String::from("."));
+    let path: String = args.nth(1).unwrap_or(String::from("."));
     match git_root(path) {
         Ok(dir) => match dir {
-            Some(root) => println!("{:?}", root),
-            None => print!("Not a git dir"),
+            Some(root) => println!("{}", root.to_str().unwrap().to_string()),
+            None => std::process::exit(1),
         },
-        Err(e) => eprintln!("Unable to check path: {}", e),
+        Err(e) => {
+            eprintln!("error: {}", e.kind());
+            std::process::exit(2);
+        }
     }
 }
