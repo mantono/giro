26cf2df267cf8f966ab3022d83de9fb44d94dc88
Anton Österberg <anton.osterberg@protonmail.com>
2023-03-07 00:17:28 +0100

    More idiomatic string handling

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use giro::git_root;
 
 fn main() {
     let mut args = std::env::args();
-    let path: String = args.nth(1).unwrap_or(String::from("."));
+    let path: String = args.nth(1).unwrap_or_else(|| String::from("."));
     match git_root(path) {
         Ok(dir) => match dir {
-            Some(root) => println!("{}", root.to_str().unwrap().to_string()),
+            Some(root) => println!("{}", root.to_str().unwrap()),
             None => std::process::exit(1),
         },
         Err(e) => {
