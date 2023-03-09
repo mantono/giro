55aee063deede9da7a1b53be3d360abd91e1a3a2
Anton Österberg <anton.osterberg@protonmail.com>
2023-03-09 02:21:33 +0100

    Complete help text

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,22 @@ use giro::git_root;
 fn main() {
     let mut args = std::env::args();
     let path: String = args.nth(1).unwrap_or_else(|| String::from("."));
+    match path.as_str() {
+        "-h" | "--help" => help(),
+        _ => resolve(&path),
+    }
+}
+
+fn help() {
+    eprintln!("git-root");
+    eprintln!("Anton Österberg <anton42x.io>\n");
+    eprintln!("USAGE:\n\tgit root <path>\n");
+    eprintln!("FLAGS:");
+    eprintln!("\t-h            print this help text");
+    eprintln!("\t--help        print man pages (if installed)")
+}
+
+fn resolve(path: &str) {
     match git_root(path) {
         Ok(dir) => match dir {
             Some(root) => println!("{}", root.to_str().unwrap()),
